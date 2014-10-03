0b8a7bc610d313aee0f16821e993ef0543860899
Christian Schulze <christian.schulze@mni.thm.de>
2014-10-03 17:34:16 +0200

    theme with bower dependencies implemented/added

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,32 @@
-console.log('\'Allo \'Allo!');
+'use strict';
+
+
+(function($) {
+    $(document).ready(function() {
+    	// bootstrap <select>
+    	$('select').selectpicker();
+
+    	// scrolling animations like parallax
+        var scrollorama = $.scrollorama({
+            blocks: '.scrollblock',
+            enablePin: false
+        });
+
+        // parallax background
+        // position
+        scrollorama.animate('#parallax-2', {
+            duration: 600,
+            property: 'top',
+            start: 0,
+            end: -100
+        });
+
+        // opacity
+        scrollorama.animate('#parallax-2', {
+        	duration: 150,
+        	property: 'opacity',
+        	start: 1,
+        	end: 0
+        });
+    });
+})(jQuery);
