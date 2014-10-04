56605c9c977ce4fca8152897ee0cdb98efbc3d66
Christian Schulze <christian.schulze@mni.thm.de>
2014-10-04 14:16:40 +0200

    added login modal with ajax post request

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -28,5 +28,25 @@
         	start: 1,
         	end: 0
         });
+
+        // sign up form
+        $('button#signupButton').click(function() {
+            $.ajax({
+                type: 'POST',
+                url: 'http://127.0.0.1:3333/attendee',
+                data: $('form-signup').serialize(),
+                success: function() {
+                    // show success info
+                    $('#modalBody').html('<div class="alert alert-success">Du bist erfolgreich angemeldet. Wir freuen uns auf Dich!</div');
+                    // replace buttons with one close button
+                    $('#modalFooter').html('<button type="button" class="btn btn-default" data-dismiss="modal">Schließen</button>');
+                },
+                error: function(res) {
+                    console.log(res);
+                    // show error
+                    $('#modalBody').html('<div class="alert alert-danger">Bei der Anmeldung ist ein Fehler aufgetreten, versuche es doch später noch einmal.</div>' + $('#modalBody').html());
+                }
+            });
+        });
     });
 })(jQuery);
